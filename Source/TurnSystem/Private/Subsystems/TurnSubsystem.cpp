4a0e45ea9902d4c01eb152c2153636f1a53246bf
fabriciomsantos <fabriciomoreira19@gmail.com>
2021-06-22 14:49:49 -0300

    Updated file name

--- Source/TurnSystem/Private/Subsystems/TurnSubsystem.cpp
+++ Source/TurnSystem/Private/Subsystems/TurnSubsystem.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Subsystems/TurnSubsystem.h"
-#include "TurnLog.h"
+#include "TurnSystemLog.h"
 
 UTurnSubsystem::UTurnSubsystem()
 {
